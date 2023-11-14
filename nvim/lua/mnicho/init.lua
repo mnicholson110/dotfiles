298e4eade6d537635460c282efda7412017b74d5
matt <mnicholson110@gmail.com>
2023-11-14 00:39:21 -0600

    clean

---
+++ nvim/lua/mnicho/init.lua
@@ -0,0 +1,75 @@
+require("mnicho.remap")
+require("mnicho.set")
+require("mnicho.packer")
+
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "rust_analyzer", "pylsp", "gopls", "lua_ls" },
+})
+
+require("mason-lspconfig").setup_handlers {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function (server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {}
+  end,
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  ["rust_analyzer"] = function ()
+     require("rust-tools").setup()
+  end,
+  ["gopls"] = function ()
+    require("lspconfig").gopls.setup({settings = {
+                             gopls = {
+                               gofumpt = true,
+                             }}})
+  end,
+}
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered('rounded'),
+    documentation = cmp.config.window.bordered('rounded'),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = nil,
+    ['<S-Tab>'] = nil,
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+require('lualine').setup()
+
+require('transparent').setup()
+
+require('harpoon').setup({
+    global_settings = {
+        tabline = true,
+        tabline_auto_open = true,
+        tabline_position = 'top',
+        tabline_prefix = ' ',
+        tabline_suffix = ' ',
+    },
+})
+
+vim.opt.cmdheight=0
+
+vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd TermOpen * setlocal nonumber norelativenumber]]
---
+++ nvim/lua/mnicho/packer.lua
@@ -0,0 +1,58 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use('wbthomason/packer.nvim')
+
+  use({
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  })
+
+  use({
+    'Mofiqul/dracula.nvim',
+     as = 'dracula',
+     config = function()
+       vim.cmd('colorscheme dracula')
+     end,
+  })
+
+  use({'nvim-treesitter/nvim-treesitter',
+      run = function()
+        local ts_update = require('nvim-treesitter.install').update({with_sync = true})
+        ts_update()
+      end,}
+  )
+  use('theprimeagen/harpoon')
+  use('github/copilot.vim')
+  use({
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+  })
+  use({
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+    }
+  })
+  use('theprimeagen/vim-be-good')
+  use('simrat39/rust-tools.nvim')
+
+  use({
+    'nvim-lualine/lualine.nvim',
+    requires = {'nvim-tree/nvim-web-devicons', opt = true}
+  })
+
+  use('xiyaowong/transparent.nvim')
+
+
+end)
