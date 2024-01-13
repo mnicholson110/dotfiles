f5f90bcda252ccea9c7a7c57a51bd2d54fefbc93
Matt <mnicholson110@gmail.com>
2024-01-12 21:12:14 -0600

    updates, idk

--- nvim/lua/mnicho/remap.lua
+++ nvim/lua/mnicho/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = true })
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = true })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
