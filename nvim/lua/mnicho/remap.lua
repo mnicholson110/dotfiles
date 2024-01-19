29befd658b86f8fd8a4c2af01a1cd298d5eb2f42
Matt <mnicholson110@gmail.com>
2024-01-18 22:22:57 -0600

    update nvim keybinds

--- nvim/lua/mnicho/remap.lua
+++ nvim/lua/mnicho/remap.lua
@@ -10,3 +10,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-Return>", ":vsplit<CR>")
--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -4,4 +4,5 @@ export ZSH="$HOME/.oh-my-zsh"
 export ZSH_CUSTOM="$HOME/.dotfiles/oh-my-zsh"
 ZSH_THEME="mnich"
 plugins=(git)
+alias tt="tt -showwpm"
 source $ZSH/oh-my-zsh.sh
