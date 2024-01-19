735819de8e77aa17ceafc5aae146912dbebe0bef
Matt <mnicholson110@gmail.com>
2024-01-18 23:47:00 -0600

    update nvim bindings

--- nvim/lua/mnicho/remap.lua
+++ nvim/lua/mnicho/remap.lua
@@ -2,11 +2,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = true })
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = true })
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-Return>", ":vsplit<CR>")
--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -4,4 +4,6 @@ export ZSH="$HOME/.oh-my-zsh"
 export ZSH_CUSTOM="$HOME/.dotfiles/oh-my-zsh"
 ZSH_THEME="mnich"
 plugins=(git)
+alias scala="scala3"
+alias scalac="scalac3"
 source $ZSH/oh-my-zsh.sh
