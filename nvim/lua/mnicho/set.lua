4698891d55da9e544be002c5e3a7d91a98db0df6
Matt <mnicholson110@gmail.com>
2024-04-08 19:53:06 -0500

    folding to nvim

--- nvim/lua/mnicho/set.lua
+++ nvim/lua/mnicho/set.lua
@@ -21,3 +21,8 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldenable = true 
+
