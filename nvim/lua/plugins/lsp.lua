eeff630ae6e119eea19e5a338c52975f790aecb6
matt <mnicholson110@gmail.com>
2025-05-06 20:46:43 -0500

    mason-lspconfig: removed features

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,16 +8,5 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = { "clangd", "rust_analyzer", "pylsp", "gopls", "lua_ls", "jdtls" },
         })
-        require("mason-lspconfig").setup_handlers {
-            ["lua_ls"] = function()
-                require("lspconfig").lua_ls.setup({
-                    settings = {
-                        Lua = {
-                            diagnostics = { globals = { "vim" } }
-                        }
-                    }
-                })
-            end
-        }
     end,
 }
