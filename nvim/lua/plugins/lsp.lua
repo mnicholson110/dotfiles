c815bbc508835785650524818aba9db347f3dd2e
matt <mnicholson110@gmail.com>
2025-07-02 14:52:08 -0500

    idk

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,6 +7,14 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = { "clangd", "rust_analyzer", "pylsp", "gopls", "lua_ls", "jdtls" },
+            automatic_enable = {
+                exclude = { "clangd" }
+            }
         })
+        require("lspconfig").clangd.setup {
+            init_options = {
+                fallbackFlags = { '--std=c23' }
+            },
+        }
     end,
 }
