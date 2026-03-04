21bcb499b655c35fa6f24c8593839b5bf04819b7
Matt <mnicholson110@gmail.com>
2026-03-04 09:19:30 -0600

    nvim

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -5,11 +5,6 @@ vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
-vim.keymap.set("n", "<C-Return>", ":vsplit<CR>")
 
 --DB keymaps
 vim.keymap.set('n', '<leader>e', '<Plug>(DBUI_ExecuteQuery)', { noremap = true, silent = true })
