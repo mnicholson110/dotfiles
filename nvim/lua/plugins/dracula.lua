376373aa0543fe8a00e9e021908b466bcb07fd90
Matt <mnicholson110@gmail.com>
2026-03-04 09:45:33 -0600

    nvim changes

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -4,6 +4,5 @@ return {
     priority = 1000,
     config = function()
         vim.cmd([[colorscheme dracula-soft]])
-        vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#6b7089" })
     end,
 }
